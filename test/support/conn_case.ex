07db0314073c56d51eb233de87fdba934b7cd183
Mayel de Borniol <git@mayel.space>
2025-10-16 23:12:26 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1588

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -11,7 +11,7 @@ defmodule Bonfire.Gatherings.ConnCase do
   we enable the SQL sandbox, so changes done to the database
   are reverted at the end of every test. If you are using
   PostgreSQL, you can even run database tests asynchronously
-  by setting `use MyApp.Web.ConnCase, async: true`, although
+  by setting `use MyApp.Web.ConnCase, async: System.get_env("TEST_UI_ASYNC") != "no"`, although
   this option is not recommended for other databases.
   """
 
@@ -26,7 +26,7 @@ defmodule Bonfire.Gatherings.ConnCase do
       import Bonfire.UI.Common.Testing.Helpers
 
       import Phoenix.LiveViewTest
-      # import Bonfire.Gatherings.ConnCase, async: true
+      # import Bonfire.Gatherings.ConnCase, async: System.get_env("TEST_UI_ASYNC") != "no"
 
       import Bonfire.Gatherings.Test.ConnHelpers
       import Bonfire.Gatherings.Test.FakeHelpers
